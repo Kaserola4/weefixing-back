bc54e282e3f6bab07c9aae786874fea87be2f9f4
Dani <79950554+Kaserola4@users.noreply.github.com>
2021-12-08 20:00:18 -0700

    Revert "Revert "Autorización por jwt añadida + http cookie + cookie-parser""
    This reverts commit bb06436c73d399b4c9681a5120c2b36d8a5db912.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const { validateToken } = require('./src/utilities/JwtManager');
 
 const PORT = process.env.port || 3001;
 const API_ENDPOINT = process.env.API_ENDPOINT;
@@ -9,17 +11,23 @@ const API_ENDPOINT = process.env.API_ENDPOINT;
 // cors
 app.use(cors());
 
+// Cookie parsers
+app.use(cookieParser());
+
 // JSON
 app.use(express.json());
+
+// Login
+app.use(`${API_ENDPOINT}`, require('./src/routes/LoginHttpMethods'));
+
+// Register
+app.use(`${API_ENDPOINT}`, require('./src/routes/RegisterHttpMethods'));
 
 // Products routes
 app.use(`${API_ENDPOINT}`, require('./src/routes/ProductsHttpMethods'));
 
 // Users routes
 app.use(`${API_ENDPOINT}`, require('./src/routes/UsersHttpMethods'));
-
-// Cart routes
-app.use(`${API_ENDPOINT}`, require('./src/routes/CartHttpMethods'));
 
 // Categories routes
 app.use(`${API_ENDPOINT}`, require('./src/routes/CategoriesHttpMethods'));
@@ -30,8 +38,8 @@ app.use(`${API_ENDPOINT}`, require('./src/routes/CarrouselHttpMethods'));
 // Offers routes
 app.use(`${API_ENDPOINT}`, require('./src/routes/OffersHttpMethods'));
 
-// Login
-app.use(`${API_ENDPOINT}`, require('./src/routes/LogingHttpMethods'));
+// Cart routes
+app.use(`${API_ENDPOINT}`, validateToken, require('./src/routes/CartHttpMethods'));
 
 // FIRE IT UP
 app.listen(PORT, () => console.log(`Listening to http://Localhost:${PORT}`));
